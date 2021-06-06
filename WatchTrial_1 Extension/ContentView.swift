5254c07ccefadfa5ab82bf3f46cbf145f55ce916
Khanjan Vaidya <47827803+khanjan2000@users.noreply.github.com>
2021-06-06 10:29:35 +0530

    Add files via upload

---
+++ WatchTrial_1 Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  watchTrial_1 Extension
+//
+//  Created by khanjan Vaidya on 02/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WatchTrial_1 Extension/NotificationView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationView.swift
+//  watchTrial_1 Extension
+//
+//  Created by khanjan Vaidya on 02/06/21.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
